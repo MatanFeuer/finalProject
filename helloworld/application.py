f0575c0d9e875bfb6167e77c7f074c133413e244
EC2 Default User <ec2-user@ip-172-31-55-46.ec2.internal>
2021-08-20 08:06:04 +0000

    added set and delete

--- helloworld/application.py
+++ helloworld/application.py
@@ -27,6 +27,39 @@ def get_id():
     resp = table.scan()
     print(str(resp))
     return Response(json.dumps(resp['Items']), mimetype='application/json', status=200)
+    
+    
+@application.route('/set_book', methods=['GET'])
+def set_doc():
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('books')
+    cid = request.args.get('id')
+    title = request.args.get('title')
+    author = request.args.get('author')
+    item={
+    'id': cid,
+    'title': title,
+    'author': author,
+     }
+    table.put_item(Item=item)
+    
+    return Response(json.dumps(item), mimetype='application/json', status=200)
+    
+@application.route('/del_book' , methods=['GET'])
+
+def del_doc():
+    id=request.args.get('id')
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('books')
+    
+    resp = table.delete_item(
+        Key={
+            'id':id
+        }
+        )
+    print (str(resp))
+    return Response(json.dumps(resp), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
