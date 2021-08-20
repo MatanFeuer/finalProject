ae5d5578629a82b6e7b730d0b87a18bc6b22dd55
EC2 Default User <ec2-user@ip-172-31-55-46.ec2.internal>
2021-08-20 07:51:58 +0000

    added get books

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,7 +1,12 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
+import requests
+import boto3
+from flask_cors import CORS
+from datetime import datetime
+import simplejson as json
 
 application = Flask(__name__)
 
@@ -12,6 +17,16 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    
+
+@application.route('/get_books', methods=['GET'])
+def get_id():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('books')
+    # replace table scan ###
+    resp = table.scan()
+    print(str(resp))
+    return Response(json.dumps(resp['Items']), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
